8c3bd1330545438d51037e6f92eb0093b7da96d3
Corentin <corentin.apolinario@gmail.com>
2019-05-27 13:34:18 +0200

    updates

--- ChatAndroid.Chat/app/src/main/java/com/example/chat/ChoixConvActivity.java
+++ ChatAndroid.Chat/app/src/main/java/com/example/chat/ChoixConvActivity.java
@@ -1,15 +1,24 @@
 package com.example.chat;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ChoixConvActivity extends RestActivity {
 
@@ -37,7 +46,7 @@ public class ChoixConvActivity extends RestActivity {
              * */
 
             int i;
-            JSONArray convs = null;
+            JSONArray convs;
             try {
                 convs = o.getJSONArray("conversations");
                 for(i=0;i<convs.length();i++) {
@@ -45,13 +54,14 @@ public class ChoixConvActivity extends RestActivity {
 
                     int id =Integer.parseInt(nextConv.getString("id"));
                     String theme = nextConv.getString("theme");
-                    Boolean active = ((String) nextConv.getString("active")).contentEquals("1");
+                    Boolean active = (nextConv.getString("active")).contentEquals("1");
 
                     gs.alerter("Conv " + id  + " theme = " + theme + " active ?" + active);
                     Conversation c = new Conversation(id, theme, active);
 
                     listeConvs.addConversation(c);
                 }
+                remplirSpinner();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -75,5 +85,73 @@ public class ChoixConvActivity extends RestActivity {
         //On se sert des services offerts par RestActivity
         //qui propose des méthodes d'envoi des requetes asynchrones
         envoiRequete(qs, "recupConversations");
+    }
+
+    private void remplirSpinner() {
+
+        // V2 : Utilisation d'un adapteur customisé qui permet de définir nous-même
+        // la forme des éléments à afficher
+
+        Spinner sp = (Spinner) findViewById(R.id.choixConversation_choixConv);
+
+        sp.setAdapter(new MyCustomAdapter(this,
+                R.layout.spinner_item,
+                listeConvs.getList()));
+    }
+
+    public class MyCustomAdapter extends ArrayAdapter<Conversation> {
+
+        private int layoutId;
+        private ArrayList<Conversation> dataConvs;
+
+        public MyCustomAdapter(Context context,
+                               int itemLayoutId,
+                               ArrayList<Conversation> objects) {
+            super(context, itemLayoutId, objects);
+            layoutId = itemLayoutId;
+            dataConvs = objects;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            //return getCustomView(position, convertView, parent);
+            LayoutInflater inflater = getLayoutInflater();
+            View item = inflater.inflate(layoutId, parent, false);
+            Conversation nextC = dataConvs.get(position);
+
+            TextView label = (TextView) item.findViewById(R.id.spinner_theme);
+            label.setText(nextC.getTheme());
+
+            ImageView icon = (ImageView) item.findViewById(R.id.spinner_icon);
+
+            if (nextC.getActive()) {
+                icon.setImageResource(R.drawable.icon36);
+            } else {
+                icon.setImageResource(R.drawable.icongray36);
+            }
+
+            return item;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //return getCustomView(position, convertView, parent);
+            LayoutInflater inflater = getLayoutInflater();
+            View row = inflater.inflate(layoutId, parent, false);
+            Conversation nextC = dataConvs.get(position);
+
+            TextView label = (TextView) row.findViewById(R.id.spinner_theme);
+            label.setText(nextC.getTheme());
+
+            ImageView icon = (ImageView) row.findViewById(R.id.spinner_icon);
+
+            if (nextC.getActive()) {
+                icon.setImageResource(R.drawable.icon);
+            } else {
+                icon.setImageResource(R.drawable.icongray);
+            }
+
+            return row;
+        }
     }
 }
